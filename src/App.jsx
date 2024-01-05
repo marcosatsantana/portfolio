b17c53c43f3f4768e87499980d5d262c113cd1c7
Marcos <marcost.santana@hotmail.com>
2024-01-05 16:52:23 -0300

    About Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header/Header'
 import "./App.css"
 import Home from './components/Home/Home'
+import About from './components/About/About'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main className='main'>
         <Home />
+        <About/>
       </main>
     </div>
   )
