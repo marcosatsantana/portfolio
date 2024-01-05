853238491fe8c58a751886c33d38e7c531ac2d7f
Marcos <marcost.santana@hotmail.com>
2024-01-05 15:42:08 -0300

    Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
 import "./App.css"
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div>
       <Header />
+      <main className='main'>
+        <Home />
+      </main>
     </div>
   )
 }
 
-export default App
+export default App;
