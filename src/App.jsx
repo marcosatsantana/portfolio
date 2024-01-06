312ff0ba167fad91ffcf46b7f87f2d63edaa5b51
Marcos Antonio Teles de Santana <marcos.santana@CARPAL.br>
2024-01-06 11:00:04 -0300

    Services Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header/Header'
 import "./App.css"
 import Home from './components/Home/Home'
 import About from './components/About/About'
+import Skills from './components/Skills/Skills'
+import Services from './components/Services/Services'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <main className='main'>
         <Home />
         <About/>
+        <Skills />
+        <Services />
       </main>
     </div>
   )
