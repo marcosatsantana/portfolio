26a6e1b905147c5275b281867010c88bdbc929c9
marcosxk122 <marcost.santana@hotmail.com>
2024-01-06 16:27:47 -0300

    Qualification Component

---
+++ src/components/Qualification/Qualification.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import "./qualification.css"
+
+const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1)
+
+  const toggleTab = (index) => {
+    setToggleState(index)
+  }
+  return (
+    <section className="qualification section">
+      <h2 className='section__title'>Qualificações</h2>
+      <span className='section__subtitle'>Minha jornada pessoal</span>
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div className={toggleState === 2 ? "qualification__button qualification__content-active button--flex" : "qualification__button button--flex"} onClick={() => toggleTab(1)}>
+            <i className="uil uil-graduation-cap qualification__icon"></i> Educação
+          </div>
+          <div className={toggleState === 1 ? "qualification__button qualification__content-active button--flex" : "qualification__button button--flex"} onClick={() => toggleTab(2)}>
+            <i className="uil uil-briefcase-alt qualification__icon"></i> Experiencia
+          </div>
+        </div>
+        <div className="qualification__sessions">
+          <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"} >
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Ass. T.I.</h3>
+                <span className="qualification__subtitle">Carpal Tratores LTDA</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Atual
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Analista de Sistemas</h3>
+                <span className="qualification__subtitle">Drogaria Santa Marta - LTDA</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2020 - 2022
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Suporte Tecnico</h3>
+                <span className="qualification__subtitle">Thato LTDA</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Atual
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Aux. Produção</h3>
+                <span className="qualification__subtitle">Menor Aprendiz</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2020 - 2022
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Eng de Software</h3>
+                <span className="qualification__subtitle">Unicesumar</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Atual
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">WebDesigner</h3>
+                <span className="qualification__subtitle">Rocketseat</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2020 - 2022
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Analista de Banco de Dados</h3>
+                <span className="qualification__subtitle">Sesc</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Atual
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Qualification;
