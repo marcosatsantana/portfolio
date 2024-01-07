8f425d6dd160d2af7d2088184f582b2f245a498b
marcosxk122 <marcost.santana@hotmail.com>
2024-01-07 08:03:24 -0300

    Testimonials Component Withing Swiper

---
+++ src/components/Testimonials/Data.jsx
@@ -0,0 +1,31 @@
+import Image1 from "../../assets/testimonial1.png"
+import Image2 from "../../assets/testimonial2.png"
+import Image3 from "../../assets/testimonial3.png"
+
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "Marcos Teles",
+    description: `
+      Simplesmente a melhor pessoa com quem trabalhei nos últimos tempos
+    `
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "David Souza",
+    description: `
+      Simplesmente a melhor pessoa com quem trabalhei nos últimos tempos
+    `
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "Lucas Souza",
+    description: `
+      Simplesmente a melhor pessoa com quem trabalhei nos últimos tempos
+    `
+  }
+]
