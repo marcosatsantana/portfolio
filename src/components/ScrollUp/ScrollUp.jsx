26b7f86125a2e7c1cf6bc8b16995d669f362c685
Marcos Antonio Teles de Santana <marcos.santana@CARPAL.br>
2024-01-12 11:06:50 -0300

    vercel

--- src/components/ScrollUp/ScrollUp.jsx
+++ src/components/ScrollUp/ScrollUp.jsx
@@ -4,7 +4,7 @@ import "./scrollup.css"
 const ScrollUp = () => {
   window.addEventListener("scroll", function () {
     const scrollUp = document.querySelector(".scrollup");
-    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll")
+    if (this.scrollY >= 555) scrollUp.classList.add("show-scroll")
     else scrollUp.classList.remove("show-scroll");
   })
   return (
