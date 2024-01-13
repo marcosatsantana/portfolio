417258317cccd3fa338cf82cff3f35d05ea4cc2d
marcosxk122 <marcost.santana@hotmail.com>
2024-01-13 17:27:48 -0300

    MKDesigners Domain

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
             <div className="contact__card">
               <i className="bx bx-mail-send contact__card-icon"></i>
               <h3 className="contact__card-title">Email</h3>
-              <span className="contact__card-data">marcost.santana@hotmail.com</span>
+              <span className="contact__card-data">contatos@mkdesigners.com.br</span>
               <a href="mailto:meu@email.com" className="contact__button">Enviar <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
             </div>
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         <header className='header'>
             <nav className='nav container'>
                 <a href='index.html' className='nav__logo'>
-                    Marcos
+                    MKDesigners
                 </a>
                 <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className='nav__list'>
