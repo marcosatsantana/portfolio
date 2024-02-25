4c1684795c7daa739876efb110fe554111591528
marcosxk122 <marcost.santana@hotmail.com>
2024-02-25 16:15:47 -0300

    translate

--- src/components/LanguageSelector/index.jsx
+++ src/components/LanguageSelector/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ReactSVG } from 'react-svg';
 import EnglishFlag from '../../assets/usa.svg'; // Importe o SVG da bandeira para inglês
@@ -6,19 +6,29 @@ import PortugueseFlag from '../../assets/brazil.svg'; // Importe o SVG da bandei
 
 function LanguageSelector() {
     const { i18n } = useTranslation();
+    const [language, setLanguage] = useState('en');
 
     const changeLanguage = (event) => {
         i18n.changeLanguage(event);
         setLanguage(event);
     };
-    const [langague, setLanguage] = useState('en');
+
+    useEffect(() => {
+        // Função para identificar o idioma padrão do dispositivo
+        const identifyDefaultLanguage = () => {
+            const userLanguage = navigator.language.split('-')[0]; // Obtenha o idioma principal (excluindo a região)
+            setLanguage(userLanguage);
+        };
+
+        identifyDefaultLanguage(); // Chamada inicial para definir o idioma padrão
+    }, []);
 
     return (
         <div className="flex gap-2 items-center">
-            <a onClick={() => changeLanguage('en')} className={`${langague === 'en' ? 'border-b-2' : 'border-b-0'} rounded-sm p-1 cursor-pointer`}>
+            <a onClick={() => changeLanguage('en')} className={`${language === 'en' ? 'border-b-2' : 'border-b-0'} rounded-sm p-1 cursor-pointer`}>
                 <img src={EnglishFlag} style={{ width: 14, height: 14 }} alt="English" />
             </a>
-            <a onClick={() => changeLanguage('pt')} className={`${langague === 'pt' ? 'border-b-2' : 'border-b-0'} rounded-sm p-1 cursor-pointer`}>
+            <a onClick={() => changeLanguage('pt')} className={`${language === 'pt' ? 'border-b-2' : 'border-b-0'} rounded-sm p-1 cursor-pointer`}>
                 <img src={PortugueseFlag} style={{ width: 14, height: 14 }} alt="Português" />
             </a>
         </div>
