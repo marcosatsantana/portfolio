061256e17dd3b5668e8b1dec036921e34c00d702
Marcos Antonio Teles de Santana <marcos.santana@CARPAL.br>
2024-10-03 15:34:36 -0300

    backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://backend.mkdesigners.com.br/"
+  baseURL: "https://ts-vercel-marcosatsantana-marcos-projects-b670cedd.vercel.app/"
 });
